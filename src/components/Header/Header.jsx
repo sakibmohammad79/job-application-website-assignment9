b99ec98495a3886fb34a888077fe4fdc720be2d1
Mohammad Sakib <mohammadsakib7679@gmail.com>
2023-04-10 00:11:48 +0600

    Responsive Navbar are added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className='bg-black text-white font-bold'>
+            <div className='md:flex justify-around items-center pb-8 pt-4'>
+                <div className='text-4xl mb-4 md:mb-0 ml-4 md:ml-0'>
+                    <h3 className='text-orange-500'>CareerInfo</h3>
+                </div>
+                <div className='font-lg mb-4 md:mb-0 ml-4 md:ml-0'>
+                    <Link className='hover:text-orange-500' to="/">Home</Link>
+                    <Link className='mx-6 hover:text-orange-500' to="/statistics">Statistics</Link>
+                    <Link className='hover:text-orange-500' to="/appliedJobs">Applied Jobs</Link>
+                    <Link className='mx-6 hover:text-orange-500' to="/blog">Blog</Link>
+                </div>
+                <div className='ml-4 md:ml-0'>
+                    <button className='font-lg  bg-orange-500 hover:bg-orange-700 rounded-lg px-6 py-3'>Star Applying</button>
+                </div>
+            </div>
+            <div className='flex justify-center'>
+                <h3 className='text-3xl pb-10 text-orange-400'>Job Details</h3>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/main.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from './components/Layout/Layout';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Statistics from './components/Statistics/Statistics';
+import AppliedJobs from './components/AppliedJobs/AppliedJobs';
+import Blog from './components/Blog/Blog';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/statistics',
+        element: <Statistics></Statistics>
+      },
+      {
+        path: '/appliedJobs',
+        element: <AppliedJobs></AppliedJobs>
+      },
+      {
+        path: '/blog',
+        element: <Blog></Blog>
+      }
+    ]
+  }
+])
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
